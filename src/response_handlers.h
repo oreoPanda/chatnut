609b680f35b5305d5211de8e2d7656f4d71b3692
i_fly_gliders <i_fly_gliders@66.175.233.138>
2016-01-05 16:33:19 +0100

    Created some files for better code organization

---
+++ src/response_handlers.h
@@ -0,0 +1,11 @@
+/*response_handlers.h*/
+
+#ifndef RESPONSE_HANDLERS_H
+#define RESPONSE_HANDLERS_H
+
+extern void handle_buddy_is_set(char *username);
+extern void handle_lookup_success(char *contact);
+extern void handle_login_success(char *username);
+
+#endif /* RESPONSE_HANDLERS_H */
+
