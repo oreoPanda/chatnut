674534122d50c9a4f3a60aba8f4455cdce9d6648
i_fly_gliders <i_fly_gliders@66.175.233.138>
2016-01-05 16:30:03 +0100

    Created some files for better code organization

---
+++ src/user.h
@@ -0,0 +1,12 @@
+/*user.h*/
+
+#ifndef USER_H
+#define USER_H
+
+extern char *get_username(void);
+extern void set_username(char *username);
+extern char *get_buddy(void);
+extern void set_buddy(char *name);
+
+#endif /* USER_H */
+
