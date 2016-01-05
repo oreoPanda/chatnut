f2d99499875f5692a9b7673f5ae1cdcaea40607c
i_fly_gliders <i_fly_gliders@66.175.233.138>
2016-01-05 16:32:59 +0100

    Created some files for better code organization

---
+++ src/file_operations.h
@@ -0,0 +1,15 @@
+/*file_operations.h*/
+
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+#include <gtk/gtk.h>
+
+extern GtkListStore *create_contact_list_model(void);
+extern void load_history( const char *contact, char **to );
+extern void append_to_history( const char *message, const char *username );
+extern int init_chatnut_directory(void);
+extern int init_user_directory(void);
+
+#endif /* FILE_OPERATIONS_H */
+
