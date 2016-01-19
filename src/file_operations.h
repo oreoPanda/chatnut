3bebafcbf356352f4ca9aaf6b697d7cb74d59979
i_fly_gliders <i_fly_gliders@66.175.233.138>
2016-01-19 07:36:41 +0100

    Update contact list when adding contact

--- src/file_operations.h
+++ src/file_operations.h
@@ -6,6 +6,7 @@
 #include <gtk/gtk.h>
 
 extern GtkListStore *create_contact_list_model(void);
+extern void add_contact_to_list(const char *contact);
 extern void load_history( const char *contact, char **to );
 extern void append_to_history( const char *message, const char *username );
 extern int init_chatnut_directory(void);
