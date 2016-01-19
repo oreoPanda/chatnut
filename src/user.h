07d363b739ee20e97a5d2a17a6cd5ef517885666
i_fly_gliders <i_fly_gliders@66.175.233.138>
2016-01-19 07:36:58 +0100

    Update contact list when adding contact

--- src/user.h
+++ src/user.h
@@ -4,7 +4,7 @@
 #define USER_H
 
 extern char *get_username(void);
-extern void set_username(char *username);
+extern void set_username(const char *username);
 extern char *get_buddy(void);
 extern void set_buddy(char *name);
 
